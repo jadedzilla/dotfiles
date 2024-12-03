6835a3d7d121e2d431edce9577735c942383c653
jadedzilla <4943147+jadedzilla@users.noreply.github.com>
2024-12-02 18:42:45 -0700

    Add font to dwm install

--- scripts/.config/scripts/setup-dwm.sh
+++ scripts/.config/scripts/setup-dwm.sh
@@ -8,10 +8,10 @@ installDWM() {
     case "$PACKAGER" in
     pacman)
         printf "%b\n" "${YELLOW}Installing Xorg...${RC}"
-        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg-xrdb picom dex python-pywal xdotool
+        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg-xrdb picom dex python-pywal xdotool 
         printf "%b\n" "${GREEN}Xorg installed${RC}"
         printf "%b\n" "${YELLOW}Installing DWM...${RC}"
-        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel libx11 libxinerama libxft imlib2 libxcb git unzip feh mate-polkit meson libev uthash libconfig
+        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel libx11 libxinerama libxft imlib2 libxcb git unzip feh mate-polkit meson libev uthash libconfig ttf-meslo-nerd
         cd /opt && "$ESCALATION_TOOL" git clone https://github.com/jadedzilla/jaded-dwm.git && "$ESCALATION_TOOL" chown -R "$USER": ./jaded-dwm
         cd jaded-dwm && "$ESCALATION_TOOL" make clean install
         printf "%b\n" "${GREEN}DWM installed${RC}"

31eba2abe31df11c2c0f19f6f0695e81e4f8194e
jadedzilla <4943147+jadedzilla@users.noreply.github.com>
2024-12-02 17:07:29 -0700

    Add libxft dependency

--- scripts/.config/scripts/setup-st.sh
+++ scripts/.config/scripts/setup-st.sh
@@ -8,7 +8,7 @@ installSt() {
     case "$PACKAGER" in
     pacman)
         printf "%b\n" "${YELLOW}Installing St (bread)...${RC}"
-        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel git
+        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel git libxft
         cd /opt && "$ESCALATION_TOOL" git clone https://github.com/BreadOnPenguins/st.git && "$ESCALATION_TOOL" chown -R "$USER": ./st
         cd st && "$ESCALATION_TOOL" make clean install
         printf "%b\n" "${GREEN}St installed${RC}"

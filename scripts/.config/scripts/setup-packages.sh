1983620d57f276eb6e98d94c431338531edd3f60
jadedzilla <4943147+jadedzilla@users.noreply.github.com>
2024-12-02 17:41:30 -0700

    Fix packages

--- scripts/.config/scripts/setup-packages.sh
+++ scripts/.config/scripts/setup-packages.sh
@@ -8,7 +8,7 @@ installPackages() {
     case "$PACKAGER" in
     pacman)
         printf "%b\n" "${YELLOW}Installing packages...${RC}"
-        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm ${arr[*]}
+        "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm firefox neovim goxlr-utility spotify dunst vesktop flameshot obs-studio}
         printf "%b\n" "${GREEN}All packages installed${RC}"
         ;;
     *)
